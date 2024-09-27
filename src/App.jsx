b9bdb2f04980dde8320b708f0739f0b16e0bf4e2
GuiLxP <guilhermematheus166@gmail.com>
2024-09-27 20:30:22 -0300

    Final adjustment to production. Front-end.

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ export default function App() {
           style={{
             backgroundImage: `url(${BG_GRADIENT})`,
             backgroundSize: "cover",
-            backgroundPosition: "",
+            backgroundPosition: "80%",
           }}
         >
           <Navbar />
